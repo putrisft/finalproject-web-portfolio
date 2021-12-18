81c57c6ecf4fa3c81adb793b6bbfbeccea24ed7d
Putri Sarah Fransisca <putrisarahf6@gmail.com>
2021-12-19 05:22:10 +0700

    commit changes

--- FinalProject-Portfolio/assets/script/slider.js
+++ FinalProject-Portfolio/assets/script/slider.js
@@ -1,51 +1,50 @@
-let span = document.getElementById('portfolio').getElementsByTagName('span');
+let span = document.getElementById("portfolio").getElementsByTagName("span");
 
-let latest_recipe = document.getElementsByClassName('portfolio')
+let portfolio = document.getElementsByClassName("portfolio");
 
-let recipe_page = Math.ceil(latest_recipe.length/2);
+let portfolio_page = Math.ceil(portfolio.length / 2);
 
 let l = 0;
 let movePer = 30;
 let maxMove = 120;
 
-// mobile_view	
+// mobile_view
 let mob_view = window.matchMedia("(max-width: 600px)");
-if (mob_view.matches)
-{
-	movePer = 98;
-	maxMove = 700;
+if (mob_view.matches) {
+  movePer = 98;
+  maxMove = 700;
 }
 
-let moveRight = ()=>{
-	l = l + movePer;
-	if (latest_recipe == 1){
-        l = 0; 
+let moveRight = () => {
+  l = l + movePer;
+  if (portfolio == 1) {
+    l = 0;
+  }
+
+  for (const i of portfolio) {
+    if (l > maxMove) {
+      l = l - movePer;
     }
+    i.style.left = "-" + l + "%";
+  }
+};
 
-	for(const i of latest_recipe)
-	{
-		if (l > maxMove){
-            l = l - movePer;
-        }
-		i.style.left = '-' + l + '%';
-	}
-}
+let moveLeft = () => {
+  l = l - movePer;
+  if (l <= 0) {
+    l = 0;
+  }
+  for (const i of portfolio) {
+    if (portfolio_page > 1) {
+      i.style.left = "-" + l + "%";
+    }
+  }
+};
 
-let moveLeft = ()=>{
-	l = l - movePer;
-	if (l<=0){l = 0;}
-	for(const i of latest_recipe){
-		if (recipe_page>1){
-			i.style.left = '-' + l + '%';
-		}
-	}
-}
+span[1].onclick = () => {
+  moveRight();
+};
 
-span[1].onclick = ()=>{
-    moveRight();
-}
-
-span[0].onclick = ()=>{
-    moveLeft();
-}
-	
+span[0].onclick = () => {
+  moveLeft();
+};
